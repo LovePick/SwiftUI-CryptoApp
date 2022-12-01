9541d44bc0e41f1a429133ba6f66eeeef1d6ade3
pick <picknavin556@gmail.com>
2022-12-01 14:27:35 +0700

    core: get price data from api.coingecko.com

--- SwiftCrypto/SwiftCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftCrypto/SwiftCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -6,15 +6,28 @@
 //
 
 import Foundation
+import Combine
 
 class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
+    private let dataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
+    
     init() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-        }
+        addSubscribers()
     }
+    
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] (returnedCoins) in
+                // subscribe CoinDataService.allCoins
+                // when CoinDataService.allCoins data has change
+                // will notify to this
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables) // <- subscribe can cancel anytime then need referent pointer
+    }
+    
 }
