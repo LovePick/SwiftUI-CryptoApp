90b7a4d53ff84ea98e8cbb632bd70e1ecb53ef5f
pick <picknavin556@gmail.com>
2022-12-20 13:27:40 +0700

    chore: filtering data base on search bar text

--- SwiftCrypto/SwiftCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftCrypto/SwiftCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -21,11 +21,39 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
+        
+        /// Remove this coz we combine subscribe dataService.$allCoins in new function
+        /*
         dataService.$allCoins
             .sink { [weak self] (returnedCoins) in
                 // subscribe CoinDataService.allCoins
                 // when CoinDataService.allCoins data has change
                 // will notify to this
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables) // <- subscribe can cancel anytime then need referent pointer
+        
+        */
+        
+        // update allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins) // <- subscribe $searchText and dataService.$allCoins
+            .map { (text, startingCoins) -> [CoinModel] in // <- output will be string from $searchText and [CoinModel] from dataService.allCoins ( can use sink but we need to convert to filter responst [CoinModel]
+                
+                guard !text.isEmpty else {
+                    return startingCoins
+                }
+                
+                let lowercasedText = text.lowercased()
+                let filteredCoins = startingCoins.filter { (coin) -> Bool in
+                    return coin.name.lowercased().contains(lowercasedText) ||
+                    coin.symbol.lowercased().contains(lowercasedText) ||
+                    coin.id.lowercased().contains(lowercasedText)
+                }
+                
+                return filteredCoins
+            }
+            .sink { [weak self](returnedCoins) in // <- after filter return [CoinModel]
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables) // <- subscribe can cancel anytime then need referent pointer
